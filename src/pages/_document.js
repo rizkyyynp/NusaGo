daf7f1d2b4b6fa1536bbadadefc9b4eecfff556b
Rizky Nugraha <rizkynp990@gmail.com>
2024-07-07 01:11:55 +0700

    Setup Project Next

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <script src="https://kit.fontawesome.com/4eeddc5c62.js" crossorigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
