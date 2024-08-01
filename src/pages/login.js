29f684f21b807e6614198c0892b060522e9d2422
Rizky Nugraha <rizkynp990@gmail.com>
2024-08-01 19:04:20 +0700

    SEO login2

--- src/pages/login.js
+++ src/pages/login.js
@@ -10,24 +10,26 @@ export default function Login() {
         <div className="flex flex-col md:flex-row items-center justify-center min-h-screen bg-zinc-100 p-4">
             <div className="bg-white shadow-lg rounded-lg overflow-hidden max-w-4xl w-full flex flex-col md:flex-row">
                 <div className="md:w-1/2 p-6 flex items-center justify-center">
-                    <Image
-                        src={LoginIcon} 
-                        alt="Login Illustration"
-                        layout='responsive'
-                        width={500}
-                        height={500}
-                        sizes="(max-width: 768px) 100vw, 50vw"
-                        priority
+                    <div className="w-full md:w-3/4">
+                        <Image
+                            src={LoginIcon}
+                            alt="Login Illustration"
+                            layout="responsive"
+                            width={500}
+                            height={500}
+                            sizes="(max-width: 768px) 100vw, 50vw"
+                            priority
                         />
+                    </div>
                 </div>
                 <div className="md:w-1/2 p-6 flex flex-col justify-center">
                     <div className="text-center mb-6">
                         <div className="flex items-center justify-center">
-                            <Image 
-                            src={NusaIcon} 
-                            alt="NusaGo Logo"
-                            width={40}
-                            height={40}
+                            <Image
+                                src={NusaIcon}
+                                alt="NusaGo Logo"
+                                width={40}
+                                height={40}
                             />
                             <h1 className="text-2xl font-extrabold text-transparent bg-clip-text bg-primary-gradient font-podkova">NusaGo | <span className='text-2xl font-extrabold text-transparent bg-clip-text bg-primary-gradient font-podkova'>Login</span></h1>
                         </div>
