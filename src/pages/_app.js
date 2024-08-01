4f95efe58ca0458c6936209d389c5b21bc013c7d
Rizky Nugraha <rizkynp990@gmail.com>
2024-08-01 18:39:13 +0700

    Add Head on app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { Provider, useSelector } from 'react-redux';
 import store from '../redux/store/store';
 import React, { useEffect } from "react";
+import Head from "next/head";
 
 function DarkModeWrapper({ children }) {
     const darkMode = useSelector((state) => state.darkMode.darkMode);
@@ -21,6 +22,11 @@ function MyApp({ Component, pageProps }) {
     return (
         <Provider store={store}>
             <DarkModeWrapper>
+                <Head>
+                    <title>NusaGo Travel</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <meta name="description" content="NusaGo Travel Website" />
+                </Head>
                 <Component {...pageProps} />
             </DarkModeWrapper>
         </Provider>
